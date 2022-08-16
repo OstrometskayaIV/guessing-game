c59d723f1ac05570a77d174b3e03f4d54367ca66
Irina Ostrometskaya <i.ostrometskaya@gmail.com>
2022-08-16 20:21:50 +0400

    feat: find solution for all 100 tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(minValue, maxValue) {
+        this.minValue;
+        this.maxValue;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        let result = Math.round((this.minValue + this.maxValue) / 2);
+        return result;
     }
 
     lower() {
-
+        return this.maxValue =  Math.round((this.minValue + this.maxValue) / 2);
     }
 
     greater() {
-
+        return this.minValue = Math.round((this.minValue + this.maxValue) / 2);
     }
 }
 
